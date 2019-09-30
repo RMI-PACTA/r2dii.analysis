b7ab2e0bc9522c250db0119080d6a43660d6df37
Clare2D <32903584+Clare2D@users.noreply.github.com>
2019-09-30 16:24:52 +0200

    Testing

---
+++ R/portfolio_import.R
@@ -0,0 +1,14 @@
+#' A roxygen skeleton added with "Code > Insert Roxygen Skeleton"
+#'
+#' A paragraph giving a description.
+#'
+#' Optionally, one or more paragraphs givin details.
+#'
+#' @return A string giving a message.
+#' @export
+#'
+#' @examples
+#' portfolio_import()
+portfolio_import <- function() {
+  message("Demo.")
+}

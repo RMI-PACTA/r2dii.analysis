25075a3875682d08f12da9c8f5e8ad00c6cf9a25
Mauro Lepore <maurolepore@gmail.com>
2019-11-07 10:19:55 -0300

    Revert "First push of functions"
    This reverts commit 5e949460bc362d4ef6542cf25c66de8dd1632567.

---
+++ R/a_function.R
@@ -0,0 +1,14 @@
+#' A roxygen skeleton added with "Code > Insert Roxygen Skeleton"
+#'
+#' A paragraph giving a description.
+#'
+#' Optionally, one or more paragraphs givin details.
+#'
+#' @return A string giving a message.
+#' @export
+#'
+#' @examples
+#' a_function()
+a_function <- function() {
+  message("Demo.")
+}

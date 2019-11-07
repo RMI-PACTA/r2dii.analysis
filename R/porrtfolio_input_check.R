3943d315d6cab5c5c0f53e26e5012b42cf8a654d
Mauro Lepore <maurolepore@gmail.com>
2019-11-07 12:01:25 -0300

    Move portfolio_input_check() to file with its name
    This commit renames a file to match the name of a main
    function, and move that main function to such file.
    
    It's common practice for a main function to live in a file
    with a matching name. For reference see the guidelines below,
    which donn't exactly match this case but close enough.
    
    > * If a file contains a single function, give the file the
        same name as the function.
    > * If a file contains multiple related functions, give it a
         concise, but evocative name.
    >
    > -- https://style.tidyverse.org/package-files.html

--- R/porrtfolio_input_check.R
+++ R/porrtfolio_input_check.R
@@ -1,3 +1,95 @@
+portfolio_input_check <- function(){
+
+  portfolio <- read_raw_portfolio_file(project_name)
+
+  portfolio <- clean_colnames_portfolio_input_file(portfolio)
+
+  portfolio <- rename_portfolio_columns(portfolio)
+
+  portfolio <- clear_portfolio_input_blanks(portfolio)
+
+  portfolio <- add_meta_portfolio(inc_metaportfolio, inc_project_metaportfolio)
+
+  portfolio <- add_holding_id(portfolio)
+
+  portfolio <- check_missing_cols(portfolio)
+
+  portfolio <- clean_portfolio_col_types(portfolio)
+
+  portfolio <- convert_currencies(portfolio, currencies)
+
+  cols_portfolio_no_bbg <- colnames(portfolio)
+  cols_funds <- c("direct_holding", "fund_isin","original_value_usd")
+
+  # Add financial data
+  # Merges in the clean data and calculates the marketvalue and number of shares
+  portfolio <- add_fin_data(portfolio, fin_data)
+
+  portfolio <- calculate_value_usd_with_fin_data(portfolio)
+
+  original_value_usd = sum(portfolio$value_usd, na.rm = T)
+
+  # identify funds in the portfolio
+  fund_portfolio <- identify_fund_portfolio(portfolio)
+
+  # Creates the fund_portfolio to match the original portfolio
+  fund_portfolio <- calculate_fund_portfolio(fund_portfolio, fund_data)
+
+  # Merges in the bbg data to the fund portfolio
+  fund_portfolio <- add_fin_data(fund_portfolio, fin_data)
+
+  # add fund_portfolio and check that the total value is the same
+  portfolio_total <- add_fund_portfolio(portfolio, fund_portfolio)
+
+  if(round(sum(portfolio_total$value_usd, na.rm = T),1) != round(original_value_usd,1)){stop("Fund Portfolio introducing errors in total value")}
+
+
+  ### TO DO
+  # summarise fund results
+  # create summary of results
+  #   use summary that already should exist
+  summarise_fund_info <- function(fund_data){
+
+
+
+
+  }
+  #   use summary data frame that's already created
+  #   check for funds with no bbg info
+  identify_missing_funds <- function(portfolio_total, fund_data){}
+  ###
+
+  # Keep going with portfolio_total
+
+  ### FLAGS/Exclusions
+
+  portfolio_total <- check_isin_format(portfolio_total)
+  portfolio_total <- check_missing_currency(portfolio_total)
+  portfolio_total <- check_valid_input_value(portfolio_total)
+  portfolio_total <- check_bloomberg_data(portfolio_total)
+
+  portfolio_total <- add_flags(portfolio_total)
+  portfolio_total <- overall_validity_flag(portfolio_total)
+
+
+  portfolio_overview <- portfolio_summary(portfolio_total)
+  # Missing is still flags about whether an isin is linked to ald or data or not.
+  # these tend to confuse, without more specific information about what sector the asset level data is in
+  # option to add in technology information from datastore?
+
+
+  ### Create portfolios to print.
+
+  ### Revenue Data
+  # This needs to be an optional
+
+
+
+
+
+
+}
+
 ### Portfolio cleaning functions
 read_raw_portfolio_file <- function(project_name){
 

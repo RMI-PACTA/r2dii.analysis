f0f2052a70e910c79436c9c703f5536ffe0b15a9
Mauro Lepore <maurolepore@gmail.com>
2019-11-11 19:06:57 -0300

    Code copied from r2dii.utils moves to R/imports.R
    R/imports.R seems like a better place for these functions, until
    they finally become directly available via r2dii.utils
    (https://github.com/2DegreesInvesting/r2dii.utils/issues/6).

--- R/utils.R
+++ R/utils.R
@@ -1,33 +1,5 @@
 # Functions that get parameters from the configuration file ---------------
 
-# FIXME: The lowercase names should be defined in r2dii.utils (r2dii.utils#6)
-financial_timestamp <- r2dii.utils::FINANCIAL.TIMESTAMP
-ald_timestamp <- r2dii.utils::ALD.TIMESTAMP
-datastore_timestamp <- r2dii.utils::DATASTORE.TIMESTAMP
-dataprep_timestamp <- r2dii.utils::DATAPREP.TIMESTAMP
-start_year <- r2dii.utils::START.YEAR
-time_horizon <- r2dii.utils::TIME.HORIZON
-risk_year <- r2dii.utils::RISK.YEAR
-additional_year <- r2dii.utils::ADDITIONAL.YEAR
-tech_list <- r2dii.utils::TECH.LIST
-tech_exclude <- r2dii.utils::TECH.EXCLUDE
-sector_list <- r2dii.utils::SECTOR.LIST
-other_sector_list <- r2dii.utils::OTHER.SECTOR.LIST
-scenario_sources_list <- r2dii.utils::SCENARIO.SOURCES.LIST
-iea_scenario_list <- r2dii.utils::IEA.SCENARIO.LIST
-web_region_list <- r2dii.utils::WEB.REGION.LIST
-scenario_geographies_list <- r2dii.utils::SCENARIO.GEOGRAPHIES.LIST
-equity_market_list <- r2dii.utils::EQUITY.MARKET.LIST
-global_aggregate_sector_list <- r2dii.utils::GLOBAL.AGGREGATE.SECTOR.LIST
-global_aggregate_scenario_sources_list <-
-  r2dii.utils::GLOBAL.AGGREGATE.SCENARIO.SOURCES.LIST
-meta_investor_name <- r2dii.utils::META.INVESTOR.NAME
-meta_portfolio_name <- r2dii.utils::META.PORTFOLIO.NAME
-has_risk <- r2dii.utils::HasRISK
-has_bv <- r2dii.utils::HasBV
-has_map <- r2dii.utils::HasMAP
-has_sb <- r2dii.utils::HasSB
-
 # These functions have a different structure.
 project_meta_investor_name <- function(inc_project_metaportfolio) {
   if (is.logical(inc_project_metaportfolio) && inc_project_metaportfolio) {

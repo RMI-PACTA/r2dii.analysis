aac9d10e8298670e9b08cbfbef1f86995488a6e8
Mauro Lepore <maurolepore@gmail.com>
2019-11-13 07:23:31 -0300

    Test with_path_in_10_projects()

---
+++ tests/testthat/test-with_path_in_10_projects.R
@@ -0,0 +1,12 @@
+test_that("with_path_in_10_projects returns the expected path", {
+  expect_is(with_path_in_10_projects(), "function")
+
+  actual <- with_path_in_10_projects("a-dir")()
+  expected <- fs::path(r2dii.utils::dbox_port2_10proj(), "a-dir")
+
+  expect_equal(actual, expected)
+
+  expect_true(
+    grepl(".*Dropbox .2. Investing..PortCheck_v2.10_Projects.a-dir$", actual)
+  )
+})

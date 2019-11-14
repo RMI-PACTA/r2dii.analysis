6234a5072dd5a676cac32f22d9dd8d089a9e7672
Mauro Lepore <maurolepore@gmail.com>
2019-11-14 18:43:46 -0300

    clean_colnames() -> clean_column_names()

--- tests/testthat/test-portfolio_input_check.R
+++ tests/testthat/test-portfolio_input_check.R
@@ -1,7 +1,7 @@
 test_that("multiplication works", {
   portfolio <- read_raw_portfolio("TEST")
-  out <- clean_colnames(portfolio)
-  expect_is(out, "data.frame")
+  out_clean_colnames <- clean_colnames(portfolio)
+  expect_is(out_clean_colnames, "data.frame")
 
   # FIXME:
   # "Investor.Name" becomes "InvestorName" but "Portfolio.Name" stays the same
@@ -9,7 +9,9 @@ test_that("multiplication works", {
   expect_false(
     identical(
       names(portfolio),
-      names(out)
+      names(out_clean_colnames)
     )
   )
+
+
 })

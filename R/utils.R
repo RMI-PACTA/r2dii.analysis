f7f083ad62aed4673e23397e8b220c988038f4cb
Mauro Lepore <maurolepore@gmail.com>
2019-11-14 09:47:39 -0300

    is_blank_na(): Simplify body

--- R/utils.R
+++ R/utils.R
@@ -47,15 +47,9 @@ is_dataframe_with_some_row <- function(data) {
   is.data.frame(data) && nrow(data) > 0L
 }
 
-# Checks whether a value is null or blank
 is_blank_na <- function(x) {
   has_length_1 <- identical(length(x), 1L)
   stopifnot(has_length_1)
 
-  if (is.na(x) | x == "") {
-    flag <- TRUE
-  } else {
-    flag <- FALSE
-  }
-  flag
+  is.na(x) || identical(x, "")
 }

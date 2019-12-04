553bdef83621e0477a53fddeb93dbd315d706b8f
Mauro Lepore <maurolepore@gmail.com>
2019-12-04 08:45:15 -0600

    Fix data/: "NaN" -> `NA` and `Year` is now integer

--- data-raw/SampleSDA_data_market_portfolio.R
+++ data-raw/SampleSDA_data_market_portfolio.R
@@ -1,9 +1,16 @@
 # Source: @vintented via https://github.com/2DegreesInvesting/r2dii.analysis/
 #   issues/6#issuecomment-561543097
+
+read_and_sanitize <- function(path, sheet) {
+  out <- readxl::read_excel(path, sheet = sheet, na = c("", "NaN"))
+  out$Year <- as.integer(out$Year)
+  out
+}
+
 path <- here::here("data-raw", "SampleSDA_data.xlsx")
 
-portfolio <- readxl::read_excel(path, sheet = "Portfolio")
+portfolio <- read_and_sanitize(path, sheet = "Portfolio")
 usethis::use_data(portfolio, overwrite = TRUE)
 
-market <- readxl::read_excel(path, sheet = "Market")
+market <- read_and_sanitize(path, sheet = "Market")
 usethis::use_data(market, overwrite = TRUE)

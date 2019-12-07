e69252d42692e8fffaf5e9b5f9ad80085d91530b
Mauro Lepore <maurolepore@gmail.com>
2019-12-07 06:13:59 -0600

    sda_calculation() is now last version by @vintented
    RE
    https://github.com/2DegreesInvesting/r2dii.analysis/issues/6#issuecomment-562132196

--- R/sda_calculation.R
+++ R/sda_calculation.R
@@ -1,47 +1,62 @@
-#' @export
-sda_calculation <- function(market, port, ref_sectors = c("Cement", "Steel"), ref_scenario = "SDS", start_year = 2019, target_year = 2050)  {
+sda_calculation <- function(market_data, port_data, ref_sector = c("Cement", "Steel"), ref_scenario = "B2DS", ref_geography = "Global", start_year = 2019, target_year = 2040)  {
 
-  startender <- function(input, var = Plan.Sec.EmissionsFactor, sectors = ref_sectors, year = start_year, scenario = ref_scenario) {
+  startender <- function(input_data, var = Plan.Sec.EmissionsFactor, year = start_year) {
+
     var <- enquo(var)
 
-    output <- input %>%
-      dplyr::filter(!is.nan(!!var) & Year == year & Scenario %in% scenario & Sector %in% sectors & ScenarioGeography == "Global") %>%
-      dplyr::rename(CI = !!var) %>%
-      dplyr::distinct(Investor.Name, Portfolio.Name, CI, Scenario, Sector, Allocation)
+    output_data <- input_data %>%
+      filter(!is.nan(!!var) & Year == year & Scenario %in% ref_scenario & Sector %in% ref_sector & ScenarioGeography %in% ref_geography) %>%
+      rename(CI = !!var) %>%
+      distinct(Investor.Name, Portfolio.Name, CI, Scenario, ScenarioGeography, Sector, Allocation)
 
-
-    return(output)
+    return(output_data)
   }
 
 
-  CI_port <- startender(input = port)
-  CI_market <- startender(input = market) %>%
-    dplyr::filter(Portfolio.Name == "GlobalMarket")
-  SI <- startender(input = market, year = target_year, var = Scen.Sec.EmissionsFactor) %>%
-    dplyr::filter(Portfolio.Name == "GlobalMarket") %>%
-    dplyr::rename(SI = CI)
+  CI_port <- startender(input_data = port_data)
+  CI_market <- startender(input_data = market_data)
+  SI <- startender(input_data = market_data, year = target_year, var = Scen.Sec.EmissionsFactor) %>%
+    rename(SI = CI)
 
-  Distance <- CI_port %>%
-    dplyr::inner_join(CI_market, by = c("Sector", "Scenario", "Allocation"), suffix = c("_port", "_market")) %>%
-    dplyr::inner_join(SI, by = c("Sector", "Scenario", "Allocation", "Portfolio.Name_market" = "Portfolio.Name", "Investor.Name_market" = "Investor.Name")) %>%
-    dplyr::mutate(D_port = CI_port - SI)
+  Distance <- CI_market %>%
+    inner_join(SI, by = c("Sector", "Scenario", "Allocation", "Portfolio.Name",  "Investor.Name", "ScenarioGeography")) %>%
+    inner_join(CI_port, by = c("Sector", "Scenario", "Allocation", "ScenarioGeography"), suffix = c("_market", "_port")) %>%
+    mutate(D_port = CI_port - SI)
 
-  view <- function(input = port, scenario = ref_scenario, sectors = ref_sectors) {
-    output <- input %>%
-      dplyr::filter(Scenario == scenario & Sector %in% ref_sectors) %>%
-      dplyr::distinct(Investor.Name, Portfolio.Name, Allocation, Sector, Year, Scen.Sec.EmissionsFactor)
+  view <- function(input_data = port_data) {
+
+    output_data <- input_data %>%
+      filter(Scenario %in% ref_scenario & Sector %in% ref_sector & ScenarioGeography %in% ref_geography) %>%
+      distinct(Investor.Name, Portfolio.Name, Allocation, Sector, Scenario, ScenarioGeography, Year, Scen.Sec.EmissionsFactor)
+
+    return(output_data)
   }
 
-  market <- view(input = market, scenario = ref_scenario, sectors = ref_sectors)
-  port <- view(input = port)
+  market_view <- view(input_data = market_data)
+  port_view <- view(input_data = port_data)
 
 
-  port_to_market <- port %>%
-    dplyr::inner_join(market, by = c("Sector", "Year", "Allocation"), suffix = c("_port", "_market")) %>%
-    dplyr::inner_join(Distance, by = c("Sector", "Investor.Name_port", "Portfolio.Name_port",  "Investor.Name_market", "Portfolio.Name_market", "Allocation")) %>%
-    dplyr::mutate(P_market  = (Scen.Sec.EmissionsFactor_market - SI)/(CI_market - SI)) %>%
-    dplyr::mutate(Scen.Sec.EmissionsFactor_port = (D_port*1*P_market)+SI)
+  port_to_market <- market_view %>%
+    select(-c(Investor.Name, Portfolio.Name)) %>%
+    inner_join(port_view, by = c("Sector", "Year", "Allocation", "ScenarioGeography", "Scenario"), suffix = c("_port", "_market"))
 
-  return(port_to_market)
+  port_to_distance <- port_to_market %>%
+    inner_join(Distance, by = c("Scenario", "Sector", "Investor.Name" = "Investor.Name_port", "Portfolio.Name" = "Portfolio.Name_port", "Allocation", "ScenarioGeography"))
+
+  port_calculation <- port_to_distance %>%
+    mutate(P_market  = (Scen.Sec.EmissionsFactor_market - SI)/(CI_market - SI),
+           Scen.Sec.EmissionsFactor = (D_port*1*P_market)+SI)
+
+  port_calculation <- port_calculation %>%
+    select(Investor.Name, Portfolio.Name, Allocation, Sector, Scenario, ScenarioGeography, Year, Scen.Sec.EmissionsFactor)
+
+  port_data <- port_calculation %>%
+    right_join(port_data, by = c("Investor.Name", "Portfolio.Name", "Allocation", "Scenario", "Sector", "ScenarioGeography", "Year"), suffix = c("", "_sda"))
+
+  port_data <- port_data %>%
+    mutate(Scen.Sec.EmissionsFactor = if_else(!is.na(Scen.Sec.EmissionsFactor_sda), Scen.Sec.EmissionsFactor_sda, Scen.Sec.EmissionsFactor)) %>%
+    select(-Scen.Sec.EmissionsFactor_sda)
+
+  return(port_data)
 
 }

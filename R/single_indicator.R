ec72035c4d70ee3d0bcb4189aeafd8488ddb185f
Mauro Lepore <maurolepore@gmail.com>
2020-01-20 08:47:16 -0600

    Remove outdated comment

--- R/single_indicator.R
+++ R/single_indicator.R
@@ -28,7 +28,6 @@ sector_weightings <- read_xlsx(file, sheet = "tech_sector_weighting")
 
 
 # finding the factor of the difference and the scenario production range.
-# FIXME: Extract scenario_port_relation out (to the top level)
 find_scenario_relation <- function(input,
                                    metric_name,
                                    calculation_upper,

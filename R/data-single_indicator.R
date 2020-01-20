71a5657f093f7a99c6441c5978597c3856a8faa3
Mauro Lepore <maurolepore@gmail.com>
2020-01-20 13:15:28 -0600

    Create data from raw data

---
+++ R/data-single_indicator.R
@@ -0,0 +1,15 @@
+#' Sample daset for `single_indicator()` and friends
+#'
+"tech_sector_weighting"
+
+#' Sample daset for `single_indicator()` and friends
+#'
+"scenario_relationships"
+
+#' Sample daset for `single_indicator()` and friends
+#'
+"sample_results"
+
+#' Sample daset for `single_indicator()` and friends
+#'
+"sample_audit"

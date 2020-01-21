a7d79051430af289c402ae955f490b7aae225c0d
Mauro Lepore <maurolepore@gmail.com>
2020-01-21 13:59:53 -0600

    New regression tests 4 single_indicator & friends

--- tests/testthat/test-single_indicator.R
+++ tests/testthat/test-single_indicator.R
@@ -1,21 +1,69 @@
-# Variable names as per commit sha 8923fe0 (2dii-demo_7-1_flag)
-# input_results <- r2dii.analysis::sample_results
-# input_audit <- r2dii.analysis::sample_audit
-# scenario_relationships <- r2dii.analysis::scenario_relationships
-# sector_weightings <- r2dii.analysis::tech_sector_weighting
+library(dplyr)
 
-test_that("single_indicator with provided example throws no error", {
-  expect_error(
-    single_indicator(
-      input_results = r2dii.analysis::sample_results,
-      upper_temp_threshold = 10,
-      lower_temp_threshold = 1.5,
-      start_year = 2019,
-      time_horizon = 5,
-      allocation = "PortfolioWeight",
-      group_vars = c("Investor.Name", "Portfolio.Name", "Asset.Type"),
-      scenario_relationships = r2dii.analysis::scenario_relationships
-    ),
-    NA
+test_that("single_indicator and friends yield known values", {
+  out_single_indicator <- single_indicator(
+    input_results = r2dii.analysis::sample_results,
+    upper_temp_threshold = 10,
+    lower_temp_threshold = 1.5,
+    start_year = 2019,
+    time_horizon = 5,
+    allocation = "PortfolioWeight",
+    group_vars = c("Investor.Name", "Portfolio.Name", "Asset.Type"),
+    scenario_relationships = r2dii.analysis::scenario_relationships
+  )
+
+  expect_known_value(
+    out_single_indicator,
+    "ref-single_indicator",
+    update = FALSE
+  )
+
+
+
+  out_influencemap_weighting_methodology <- influencemap_weighting_methodology(
+    input_results = out_single_indicator,
+    input_audit = r2dii.analysis::sample_audit,
+    metric_name = "temperature",
+    group_vars = c("Investor.Name", "Portfolio.Name"),
+    sector_weightings = r2dii.analysis::tech_sector_weighting
+  )
+
+  expect_known_value(
+    out_influencemap_weighting_methodology,
+    "ref-influencemap_weighting_methodology",
+    update = FALSE
+  )
+
+
+
+  out_mapped_sector_exposure <- mapped_sector_exposure(
+    input_audit = r2dii.analysis::sample_audit
+  )
+
+  expect_known_value(
+    out_mapped_sector_exposure,
+    "ref-mapped_sector_exposure",
+    update = FALSE
+  )
+
+
+
+  out_influencemap_weighting_methodology2 <-
+    out_influencemap_weighting_methodology  %>%
+    distinct(Investor.Name, Portfolio.Name, Allocation, temperature)
+
+  joint <- inner_join(
+    out_influencemap_weighting_methodology2, out_mapped_sector_exposure,
+    by = c("Investor.Name", "Portfolio.Name")
+  )
+
+  out_find_range <- find_range(joint, range = c(1.75, 2, 2.75, 3.5))
+
+  expect_known_value(
+    out_find_range,
+    "ref-out_find_range",
+    update = FALSE
   )
 })
+
+

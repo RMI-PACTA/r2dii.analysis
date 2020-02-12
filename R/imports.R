8257f705c48c301770ee68c20e483d3af96a2322
Jackson Hoffart <jackson.hoffart@gmail.com>
2020-02-12 15:33:24 +0100

    import left_join

--- R/imports.R
+++ R/imports.R
@@ -1,4 +1,4 @@
-#' @importFrom dplyr distinct filter if_else inner_join mutate rename right_join
+#' @importFrom dplyr distinct filter if_else inner_join mutate rename right_join left_join
 #' @importFrom dplyr select
 #' @importFrom rlang %||%
 NULL

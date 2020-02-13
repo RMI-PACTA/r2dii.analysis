5494f2fe95de460d0cafceb202959241062facd3
Mauro <maurolepore@gmail.com>
2020-02-13 14:36:00 -0600

    Fix tests: Don't work with fake_scenario()

--- tests/testthat/test-add_scenario_fair_shares.R
+++ tests/testthat/test-add_scenario_fair_shares.R
@@ -13,20 +13,19 @@ test_that("outputs known value (temporary regression test for refactoring)", {
 })
 
 test_that("w/ scenario with missing names errors gracefully", {
-  skip("Broken test. Skip until good data passes with no error")
   bad <- function(data, x) dplyr::rename(data, bad = x)
 
-  expect_error_missing_names <- function(scenario = fake_scenario()) {
+  expect_error_missing_names <- function(scenario) {
     expect_error(
       class = "missing_names",
       add_scenario_fair_shares(scenario, start_year = 2020)
     )
   }
 
-  expect_error_missing_names(scenario = bad(fake_scenario(), "scenario"))
-  expect_error_missing_names(scenario = bad(fake_scenario(), "sector"))
-  expect_error_missing_names(scenario = bad(fake_scenario(), "technology"))
-  expect_error_missing_names(scenario = bad(fake_scenario(), "region"))
-  expect_error_missing_names(scenario = bad(fake_scenario(), "value"))
-  expect_error_missing_names(scenario = bad(fake_scenario(), "units"))
+  expect_error_missing_names(bad(r2dii.dataraw::scenario_demo, "scenario"))
+  expect_error_missing_names(bad(r2dii.dataraw::scenario_demo, "sector"))
+  expect_error_missing_names(bad(r2dii.dataraw::scenario_demo, "technology"))
+  expect_error_missing_names(bad(r2dii.dataraw::scenario_demo, "region"))
+  expect_error_missing_names(bad(r2dii.dataraw::scenario_demo, "value"))
+  expect_error_missing_names(bad(r2dii.dataraw::scenario_demo, "units"))
 })

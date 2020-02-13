553cf7e51df1ad6154c7df26ebae446c976dd639
Mauro <maurolepore@gmail.com>
2020-02-13 14:23:34 -0600

    First successful test for basic funcionality

--- tests/testthat/test-add_scenario_fair_shares.R
+++ tests/testthat/test-add_scenario_fair_shares.R
@@ -1,5 +1,7 @@
 test_that("with fake_scenario and start_year = 2020 passes with no error", {
-  expect_no_error(add_scenario_fair_shares(scenario, start_year = 2020))
+  expect_error_free(
+    add_scenario_fair_shares(r2dii.dataraw::scenario_demo, start_year = 2020)
+  )
 })
 
 test_that("w/ scenario with missing names errors gracefully", {

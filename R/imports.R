773c7815ad08c179f93fd08aa2c0ea56dac03c57
Mauro <maurolepore@gmail.com>
2020-02-13 14:23:25 -0600

    Import dplyr::first()

--- R/imports.R
+++ R/imports.R
@@ -1,5 +1,5 @@
 #' @importFrom dplyr distinct filter if_else inner_join mutate rename right_join left_join
-#' @importFrom dplyr select one_of
+#' @importFrom dplyr select one_of first
 #' @importFrom rlang %||%
 #' @importFrom r2dii.utils check_crucial_names
 NULL

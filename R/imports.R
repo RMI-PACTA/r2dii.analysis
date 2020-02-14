926cca828a1f2ca91b966d279f74976dee33ca7a
Jackson Hoffart <jackson.hoffart@gmail.com>
2020-02-14 17:20:46 +0100

    added glue and rlang to imports

--- R/imports.R
+++ R/imports.R
@@ -1,5 +1,6 @@
 #' @importFrom dplyr distinct filter first if_else inner_join left_join mutate
 #' @importFrom dplyr  rename right_join select one_of
-#' @importFrom rlang %||%
+#' @importFrom rlang %||% abort
+#' @importFrom glue glue
 #' @importFrom r2dii.utils check_crucial_names
 NULL

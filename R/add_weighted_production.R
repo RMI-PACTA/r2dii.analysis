9ffc6b32be96ad63992724bd07177fe02bda7cba
Mauro <maurolepore@gmail.com>
2020-02-24 13:37:19 -0600

    New add_weighted_production()

---
+++ R/add_weighted_production.R
@@ -0,0 +1,80 @@
+#' Adds column `weighted_production`
+#'
+#' This function computes the weighted production of a portfolio from a matched
+#' dataset (see argument `data`). This metric aggregates the weighted production
+#' off all loans for each sector, technology, and year.
+#'
+#' @param data A dataframe like the output of [join_ald_scenario()].
+#' @param use_loan_size_credit_limit Logical. Use `TRUE` to use
+#'   the column `loan_size_credit_limit` instead of the default column
+#'   `loan_size_outstanding`.
+#'
+#' @seealso [join_ald_scenario()].
+#'
+#' @return A dataframe with the same columns as `data` and the additional
+#'   column `weighted_production`, where each row is aggregates information
+#'   of all `loan_id`s within groups defined by the unique combinations of
+#'   the columns `sector`, `technology`, and `year`.
+#'
+#' @export
+#'
+#' @examples
+#' library(r2dii.dataraw)
+#'
+#' master <- r2dii.dataraw::loanbook_demo %>%
+#'   r2dii.match::match_name(r2dii.dataraw::ald_demo) %>%
+#'   r2dii.match::prioritize() %>%
+#'   join_ald_scenario(r2dii.dataraw::ald_demo, r2dii.dataraw::scenario_demo)
+#'
+#' add_weighted_production(master)
+#'
+#' add_weighted_production(master, use_loan_size_credit_limit = TRUE)
+add_weighted_production <- function(data, use_loan_size_credit_limit = FALSE) {
+  old_groups <- dplyr::groups(data)
+
+  crucial <- c(
+    "id_loan",
+    "loan_size_credit_limit",
+    "loan_size_outstanding",
+    "production",
+    "sector",
+    "technology",
+    "year"
+  )
+  check_crucial_names(data, crucial)
+
+  loan_size <- "loan_size_outstanding"
+  if (use_loan_size_credit_limit) {
+    loan_size <- "loan_size_credit_limit"
+  }
+
+  data %>%
+    dplyr::ungroup() %>%
+
+    dplyr::group_by(.data$sector) %>%
+    dplyr::mutate(loan_size_by_sector = sum(.data[[loan_size]])) %>%
+    dplyr::ungroup() %>%
+
+    dplyr::group_by(.data$id_loan) %>%
+    dplyr::mutate(
+      loan_size_by_sector_w = .data[[loan_size]] / .data$loan_size_by_sector
+    ) %>%
+    dplyr::ungroup() %>%
+
+    dplyr::mutate(
+     production_proxy_w = .data$loan_size_by_sector_w * .data$production
+    ) %>%
+    dplyr::ungroup() %>%
+
+    dplyr::group_by(.data$sector, .data$technology, .data$year) %>%
+    mutate(weighted_production = sum(.data$production_proxy_w)) %>%
+    dplyr::ungroup() %>%
+
+    dplyr::select(
+      -.data$loan_size_by_sector,
+      -.data$loan_size_by_sector_w,
+      -.data$production_proxy_w
+    ) %>%
+
+    dplyr::group_by(!!!old_groups)
+}
---
+++ tests/testthat/test-add_weighted_production.R
@@ -0,0 +1,78 @@
+library(r2dii.dataraw)
+
+# master <- r2dii.dataraw::loanbook_demo %>%
+#   r2dii.match::match_name(r2dii.dataraw::ald_demo) %>%
+#   r2dii.match::prioritize() %>%
+#   join_ald_scenario(r2dii.dataraw::ald_demo, r2dii.dataraw::scenario_demo) %>%
+#   dplyr::slice(1) %>%
+#   dplyr::select(
+#     c(
+#       "sector",
+#       "id_loan",
+#       "loan_size_outstanding",
+#       "loan_size_credit_limit",
+#       "production",
+#       "year",
+#       "technology"
+#     )
+#   )
+# master$loan_size_outstanding <- 1
+# master$loan_size_credit_limit <- 2
+# master$production <- 1
+master <- tibble::tibble(
+  sector = "automotive",
+  id_loan = "L151",
+  loan_size_outstanding = 1,
+  loan_size_credit_limit = 2,
+  production = 1,
+  year = 2020,
+  technology = "ice"
+)
+
+test_that("with data lacking crucial columns errors with informative message", {
+  # FIXEM: Check more crucial names
+  bad <- function(data, x) dplyr::rename(data, bad = x)
+
+  expect_error(
+    class = "missing_names",
+    add_weighted_production(bad(master, "sector"))
+  )
+
+  expect_error(
+    class = "missing_names",
+    add_weighted_production(bad(master, "loan_size_outstanding"))
+  )
+
+  expect_error(
+    add_weighted_production(
+      bad(master, "loan_size_credit_limit"),
+      use_loan_size_credit_limit = TRUE
+    )
+  )
+})
+
+test_that("add_weighted_production outputs a dataframe", {
+  expect_is(add_weighted_production(master), "tbl_df")
+})
+
+test_that("with grouped data returns same groups as input", {
+  out <- master %>%
+    dplyr::group_by(.data$sector) %>%
+    add_weighted_production()
+
+  expect_equal(dplyr::group_vars(out), "sector")
+})
+
+test_that("FIXME: test is sensitive. defaults to using `loan_size_outstanding`", {
+
+  expect_error_free(
+    add_weighted_production(master)
+  )
+  expect_error_free(
+    add_weighted_production(master, use_loan_size_credit_limit = TRUE)
+  )
+})
+
+test_that("outputs new column `weighted_production`", {
+  expect_true(has_name(add_weighted_production(master), "weighted_production"))
+})

310033ad971dc5ec981f83b31ab3e21a9b46fdf6
Mauro Lepore <maurolepore@gmail.com>
2020-03-24 15:17:58 -0500

    Use snake_case (tidyverse style guide)

--- R/summarize_weighted_production.R
+++ R/summarize_weighted_production.R
@@ -55,7 +55,7 @@ add_weighted_loan_production <- function(data, use_credit_limit = FALSE) {
   )
   check_crucial_names(data, crucial)
 
-  check_crucial_NAs(data, crucial)
+  check_crucial_nas(data, crucial)
 
   old_groups <- dplyr::groups(data)
   data <- dplyr::ungroup(data)
@@ -98,16 +98,16 @@ check_unique_loan_size_values_per_id_loan <- function(data) {
   invisible(data)
 }
 
-check_crucial_NAs <- function(data, crucial) {
+check_crucial_nas <- function(data, crucial) {
   for (column in crucial) {
-    check_column_for_NAs(data, column)
+    check_column_for_nas(data, column)
   }
 }
 
-check_column_for_NAs <- function(data, column) {
+check_column_for_nas <- function(data, column) {
   if (any(is.na(data[, column]))) {
     rlang::abort(
-      class = "column_has_NAs",
+      class = "column_has_na",
       # TODO: Print which column contains `NA`
       glue::glue(
         "Column `{column}` must not contain any `NA`s."

134760b668ac25757f8e03c24b7d7c9724f0a101
Jackson Hoffart <jackson.hoffart@gmail.com>
2020-03-27 17:26:36 +0100

    add new simple tests for summarize_* functions

--- tests/testthat/test-summarize_portfolio_production.R
+++ tests/testthat/test-summarize_portfolio_production.R
@@ -5,3 +5,28 @@ test_that("with fake data outputs known value", {
 
   expect_known_value(out, "ref-summarize_portfolio_production", update = FALSE)
 })
+
+test_that("with known input outputs as expected", {
+  # styler: off
+  data <- fake_master(
+    technology =            c("ta", "ta", "tb", "tb"),
+    id_loan    =            c("i1", "i2", "i1", "i2"),
+    loan_size_outstanding = c( 40,   10,   40,   10),
+    production            = c( 10,   30,   20,   40),
+  )
+  out1 <- summarize_portfolio_production(data)
+  out1$weighted_production
+  expect_equal(out1$weighted_production, c(14, 24))
+
+  # Is sensitive to `use_credit_limit`
+  # Reversing loan_size and production outputs reverse result
+  data2 <- fake_master(
+    technology =             c("ta", "ta", "tb", "tb"),
+    id_loan    =             c("i1", "i2", "i1", "i2"),
+    loan_size_credit_limit = c( 10,   40,   10,   40),
+    production            =  c( 40,   20,   30,   10),
+  )
+  out2 <- summarize_portfolio_production(data2, use_credit_limit = TRUE)
+  expect_equal(out2$weighted_production, c(24, 14))
+  # styler: on
+})

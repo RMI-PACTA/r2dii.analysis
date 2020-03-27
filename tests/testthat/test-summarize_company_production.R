53b57e8e8f5ed31ab325009cebed11e60da7721c
Jackson Hoffart <jackson.hoffart@gmail.com>
2020-03-27 17:19:08 +0100

    add summarize_ tests with known output

--- tests/testthat/test-summarize_company_production.R
+++ tests/testthat/test-summarize_company_production.R
@@ -1,1 +1,7 @@
+test_that("with fake data outputs known value", {
+  out <- summarize_company_production(
+    fake_master()
+  )
 
+  expect_known_value(out, "ref-summarize_company_production", update = FALSE)
+})
--- tests/testthat/test-summarize_portfolio_production.R
+++ tests/testthat/test-summarize_portfolio_production.R
@@ -1,1 +1,7 @@
+test_that("with fake data outputs known value", {
+  out <- summarize_portfolio_production(
+    fake_master()
+  )
 
+  expect_known_value(out, "ref-summarize_portfolio_production", update = FALSE)
+})

6146a203f7347f2bee6b1e1a610f88bdfc8d3cfa
Mauro Lepore <maurolepore@gmail.com>
2020-04-21 12:39:10 -0500

    Style

--- tests/testthat/test-summarize_portfolio_production.R
+++ tests/testthat/test-summarize_portfolio_production.R
@@ -1,7 +1,5 @@
 test_that("with fake data outputs known value", {
-  out <- summarize_portfolio_production(
-    fake_master()
-  )
+  out <- summarize_portfolio_production(fake_master())
 
   expect_known_value(out, "ref-summarize_portfolio_production", update = FALSE)
 })

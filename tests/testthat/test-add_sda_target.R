6ec43926df6d979a04aa883269f69f57655ec138
Jackson Hoffart <jackson.hoffart@gmail.com>
2020-05-20 17:33:09 +0200

    add first sda_target test

---
+++ tests/testthat/test-add_sda_target.R
@@ -0,0 +1,13 @@
+test_that("with fake data outputs known value", {
+  out <- add_sda_target(
+    fake_matched(sector = "cement",
+                 sector_ald = "cement"),
+    ald = fake_ald(sector = "cement",
+                   technology = "cement",
+                   year = c(2020, 2021,2022),
+                   emission_factor = c(0.6, 0.7, 0.8)),
+    co2_intensity_scenario = r2dii.analysis::co2_intensity_scenario
+  )
+
+  expect_known_value(out, "ref-add_portfolio_target", update = F)
+})

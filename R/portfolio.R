3a069096507a9ea31ac8236a0368eb97cbf0c577
Jackson Hoffart <jackson.hoffart@gmail.com>
2020-05-28 20:29:32 +0200

    Remove sda_portfolio_target() and friends (#70)
    Closes #63 
    
    Also removes datasets `market` and `portfolio`.

--- R/portfolio.R
+++
@@ -1,8 +0,0 @@
-#' TODO \@vintented: Add title
-#'
-#' TODO \@vintented: Add description
-#'
-#' @family demo datasets
-#' @source \@vintented via <https://github.com/2DegreesInvesting/
-#' r2dii.analysis/issues/6#issuecomment-561543097>.
-"portfolio"

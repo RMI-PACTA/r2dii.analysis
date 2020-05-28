42342f81af94c4530c5828eefcf7ccba6b9c7487
Mauro Lepore <maurolepore@gmail.com>
2020-05-28 18:49:18 -0500

    Reorganize tests

--- tests/testthat/test-join_ald_scenario.R
+++ tests/testthat/test-join_ald_scenario.R
@@ -2,51 +2,6 @@ library(tibble)
 library(r2dii.data)
 library(r2dii.match)
 
-test_that("w/ loanbook, ald or scenario with missing names errors gracefully", {
-  bad <- function(data, x) dplyr::rename(data, bad = x)
-
-  expect_error_missing_names <- function(match_result = fake_matched(),
-                                         ald = fake_ald(),
-                                         scenario = fake_scenario()) {
-    expect_error(
-      class = "missing_names",
-      join_ald_scenario(match_result, ald, scenario)
-    )
-  }
-
-  expect_error_missing_names(match_result = bad(fake_matched(), "name_ald"))
-  expect_error_missing_names(match_result = bad(fake_matched(), "sector_ald"))
-
-  expect_error_missing_names(ald = bad(fake_ald(), "name_company"))
-  expect_error_missing_names(ald = bad(fake_ald(), "sector"))
-  expect_error_missing_names(ald = bad(fake_ald(), "technology"))
-  expect_error_missing_names(ald = bad(fake_ald(), "year"))
-
-  expect_error_missing_names(scenario = bad(fake_scenario(), "sector"))
-  expect_error_missing_names(scenario = bad(fake_scenario(), "technology"))
-  expect_error_missing_names(scenario = bad(fake_scenario(), "year"))
-
-  expect_error_missing_names(scenario = bad(fake_scenario(), "scenario_source"))
-})
-
-test_that("is sensitive to region_isos", {
-  out1 <- join_ald_scenario(
-    fake_matched(),
-    ald = fake_ald(),
-    scenario = fake_scenario(scenario_source = "weo_2019"),
-    region_isos = r2dii.data::region_isos
-  )
-
-  out2 <- join_ald_scenario(
-    fake_matched(),
-    ald = fake_ald(),
-    scenario = fake_scenario(scenario_source = "demo_2020"),
-    region_isos = r2dii.data::region_isos_demo
-  )
-
-  expect_false(identical(out1, out2))
-})
-
 test_that("with fake data outputs known value", {
   out <- join_ald_scenario(
     fake_matched(),
@@ -56,6 +11,17 @@ test_that("with fake data outputs known value", {
   )
 
   expect_known_value(out, "ref-join_ald_scenario", update = FALSE)
+})
+
+test_that("returns visibly", {
+  expect_visible(
+    join_ald_scenario(
+      fake_matched(),
+      ald = fake_ald(),
+      scenario = fake_scenario(),
+      region_isos = r2dii.data::region_isos_demo
+    )
+  )
 })
 
 test_that("outputs expected names", {
@@ -93,30 +59,22 @@ test_that("outputs expected names", {
   )
 })
 
-test_that("include/excludes `plant_location`s inside/outside a region", {
-  region_isos_toy <- tribble(
-    ~region, ~isos, ~source,
-    "north america", "cn", "demo_2020",
-    "oecd", "de", "demo_2020",
-    "oecd", "fr", "demo_2020",
-    "china", "cn", "demo_2020",
-  )
-
-  out <- join_ald_scenario(
+test_that("is sensitive to region_isos", {
+  out1 <- join_ald_scenario(
     fake_matched(),
-    # We have isos to match all countries and regions;
-    region_isos = region_isos_toy,
-    # And we have asset-level data from all countries;
-    ald = fake_ald(plant_location = c("de", "fr", "cn", "us")),
-    # But our scenario if is only relevant to Europe and China -- not US
-    scenario = fake_scenario(region = c("oecd_europe", "china"))
+    ald = fake_ald(),
+    scenario = fake_scenario(scenario_source = "weo_2019"),
+    region_isos = r2dii.data::region_isos
   )
 
-  # The output includes locations inside matching regions
-  expect_true(all(unique(out$plant_location) %in% c("de", "fr", "cn")))
+  out2 <- join_ald_scenario(
+    fake_matched(),
+    ald = fake_ald(),
+    scenario = fake_scenario(scenario_source = "demo_2020"),
+    region_isos = r2dii.data::region_isos_demo
+  )
 
-  # Excludes locations outside matching regions
-  expect_false(any(unique(out$plant_location) %in% "us"))
+  expect_false(identical(out1, out2))
 })
 
 test_that("is case-insensitive to `plant_location` inputs", {
@@ -166,6 +124,33 @@ test_that("outputs a number of rows equal to matches by `scenario_source`", {
     region_isos = tibble(isos = "a", region = "b", source = "c")
   )
   expect_equal(nrow(matching_2), 2L)
+})
+
+test_that("w/ loanbook, ald or scenario with missing names errors gracefully", {
+  bad <- function(data, x) dplyr::rename(data, bad = x)
+
+  expect_error_missing_names <- function(match_result = fake_matched(),
+                                         ald = fake_ald(),
+                                         scenario = fake_scenario()) {
+    expect_error(
+      class = "missing_names",
+      join_ald_scenario(match_result, ald, scenario)
+    )
+  }
+
+  expect_error_missing_names(match_result = bad(fake_matched(), "name_ald"))
+  expect_error_missing_names(match_result = bad(fake_matched(), "sector_ald"))
+
+  expect_error_missing_names(ald = bad(fake_ald(), "name_company"))
+  expect_error_missing_names(ald = bad(fake_ald(), "sector"))
+  expect_error_missing_names(ald = bad(fake_ald(), "technology"))
+  expect_error_missing_names(ald = bad(fake_ald(), "year"))
+
+  expect_error_missing_names(scenario = bad(fake_scenario(), "sector"))
+  expect_error_missing_names(scenario = bad(fake_scenario(), "technology"))
+  expect_error_missing_names(scenario = bad(fake_scenario(), "year"))
+
+  expect_error_missing_names(scenario = bad(fake_scenario(), "scenario_source"))
 })
 
 test_that("without `sector` throws no error", {
@@ -232,13 +217,28 @@ test_that("warns 0-rows caused by scenario or region_isos", {
   )
 })
 
-test_that("returns visibly", {
-  expect_visible(
-    join_ald_scenario(
-      fake_matched(),
-      ald = fake_ald(),
-      scenario = fake_scenario(),
-      region_isos = r2dii.data::region_isos_demo
-    )
+test_that("include/excludes `plant_location`s inside/outside a region", {
+  region_isos_toy <- tribble(
+    ~region, ~isos, ~source,
+    "north america", "cn", "demo_2020",
+    "oecd", "de", "demo_2020",
+    "oecd", "fr", "demo_2020",
+    "china", "cn", "demo_2020",
   )
+
+  out <- join_ald_scenario(
+    fake_matched(),
+    # We have isos to match all countries and regions;
+    region_isos = region_isos_toy,
+    # And we have asset-level data from all countries;
+    ald = fake_ald(plant_location = c("de", "fr", "cn", "us")),
+    # But our scenario if is only relevant to Europe and China -- not US
+    scenario = fake_scenario(region = c("oecd_europe", "china"))
+  )
+
+  # The output includes locations inside matching regions
+  expect_true(all(unique(out$plant_location) %in% c("de", "fr", "cn")))
+
+  # Excludes locations outside matching regions
+  expect_false(any(unique(out$plant_location) %in% "us"))
 })

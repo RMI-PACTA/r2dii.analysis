90f9cb3ace6d9b1e42bb2d2d9c0444f49917050c
Mauro Lepore <maurolepore@gmail.com>
2020-06-08 13:10:52 -0500

    Re-implement purrr::walk, base::isTRUE, base::isFALSE

--- R/utils.R
+++ R/utils.R
@@ -8,3 +8,20 @@ check_no_value_is_missing <- function(data, column) {
 
   invisible(data)
 }
+
+# Avoid dependency on purrr
+walk <- function(.x, .f, ...) {
+  .f <- rlang::as_function(.f)
+  lapply(.x, .f, ...)
+  invisible(.x)
+}
+
+# We can remove this once we depend on R >= 3.5. See ?backports::isTRUE
+isTRUE <- function(x) {
+  is.logical(x) && length(x) == 1L && !is.na(x) && x
+}
+
+# We can remove this once we depend on R >= 3.5. See ?backports::isFALSE
+isFALSE <- function(x) {
+  is.logical(x) && length(x) == 1L && !is.na(x) && !x
+}

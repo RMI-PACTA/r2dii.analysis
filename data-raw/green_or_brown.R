a9b8bcc1d00f9f811de0c79b9ca29dcdfed8cc79
Jackson Hoffart <jackson.hoffart@gmail.com>
2020-07-21 14:15:59 +0200

    combine internal datasets relating to tmsr_smsp

--- data-raw/green_or_brown.R
+++ data-raw/green_or_brown.R
@@ -5,11 +5,14 @@ library(usethis)
 # NOTE to 2dii internals: if you wish to set different specifications
 # for high-carbon/ low-carbon technologies, do so in this internal
 # dataset
+path_tmsr_or_smsp <- file.path("data-raw", "tmsr_or_smsp.csv")
+tmsr_or_smsp <- readr::read_csv(path_tmsr_or_smsp)
 
-path <- file.path("data-raw", "green_or_brown.csv")
-green_or_brown <- readr::read_csv(path)
+path_green_or_brown <- file.path("data-raw", "green_or_brown.csv")
+green_or_brown <- readr::read_csv(path_green_or_brown)
 
 usethis::use_data(green_or_brown,
+                  tmsr_or_smsp,
                   overwrite = TRUE,
                   internal = TRUE
 )

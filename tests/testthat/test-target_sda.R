733d5a740bfb2c9dc8ae1717cbf5923c84410220
Jackson Hoffart <jackson.hoffart@gmail.com>
2020-08-13 15:04:21 +0200

    remove error expectation if input EF is NA

--- tests/testthat/test-target_sda.R
+++ tests/testthat/test-target_sda.R
@@ -380,7 +380,6 @@ test_that("with NAs in crucial columns errors with informative message (#146)", 
 
   expect_error_crucial_NAs_ald("name_company")
   expect_error_crucial_NAs_ald("production")
-  expect_error_crucial_NAs_ald("emission_factor")
   expect_error_crucial_NAs_ald("sector")
   expect_error_crucial_NAs_ald("year")
 

f25e1748a83e26d2d38f9caefa0a2aa60541a393
Jackson Hoffart <jackson.hoffart@gmail.com>
2020-08-14 13:45:44 +0200

    add failing tests

--- tests/testthat/test-target_market_share.R
+++ tests/testthat/test-target_market_share.R
@@ -311,3 +311,18 @@ test_that("corporate economy benchmark only aggregates ultimate owners (#103)", 
 
   expect_equal(corporate_economy_value$weighted_production_value, c(50, 100))
 })
+
+test_that(
+  "`sector` column is not used from data (should only use `sector_ald`) (#178)",
+  {
+
+    expect_error_free(
+      target_market_share(
+        fake_matched() %>% select(-sector),
+        fake_ald(),
+        fake_scenario(),
+        region_isos_demo
+      )
+    )
+
+})

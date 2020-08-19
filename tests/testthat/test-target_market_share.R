a7e30bd74391bf000cdbc2b674e30038335f362e
Jackson Hoffart <jackson.hoffart@gmail.com>
2020-08-19 14:46:57 +0200

    test that `by_company` and `weight_production` warns

--- tests/testthat/test-target_market_share.R
+++ tests/testthat/test-target_market_share.R
@@ -361,3 +361,18 @@ test_that("outputs known value with `weight_production` (#131)", {
 
   expect_equal(out_unweighted$projected$production_value, 3)
 })
+
+test_that("warns if `by_company` and `weight_production` (#165)", {
+
+  expect_warning(
+      target_market_share(
+        fake_matched(),
+        ald = fake_ald(),
+        scenario = fake_scenario(),
+        region_isos = region_isos_demo,
+        by_company = TRUE,
+        weight_production = TRUE
+        )
+    )
+
+})

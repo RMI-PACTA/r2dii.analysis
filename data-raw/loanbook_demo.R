8f693e893d9c66790e03510676dda0276ee91aea
Jackson Hoffart <jackson.hoffart@gmail.com>
2020-11-26 13:42:03 +0100

    add stable internal loanbook_demo
    this loanbook_demo is stable as of v0.1.4 of r2dii.data

---
+++ data-raw/loanbook_demo.R
@@ -0,0 +1,31 @@
+# The purpose of this stable version of loanbook_demo is to mask the,
+# potentially unstable data coming from r2dii.data, for the purpose of testing.
+# In particular, the `sector` and `borderline` values are subject to change if
+# the classification system is updated, and this gives many false positives in
+# our regression tests. See the convo here:
+# https://github.com/2DegreesInvesting/r2dii.analysis/issues/227
+
+r2dii_data_installed <- requireNamespace("r2dii.data", quietly = TRUE)
+
+if (r2dii_data_installed){
+  version <- packageVersion("r2dii.data")
+}
+
+install_version("r2dii.data", version = "0.1.4")
+
+library(r2dii.data)
+library(dplyr)
+
+loanbook_demo <- loanbook_demo %>%
+  left_join(
+    select(nace_classification, code, sector, borderline),
+    by = c(sector_classification_direct_loantaker = "code")
+  )
+
+usethis::use_data(loanbook_demo, internal = TRUE)
+
+if (r2dii_data_installed) {
+  install_version("r2dii.data", version = version)
+} else {
+  remove.packages("r2dii.data")
+}

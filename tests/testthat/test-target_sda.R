fd51abed43ffc13af2ffb289fdfd9667f7abb943
Jackson Hoffart <jackson.hoffart@gmail.com>
2020-12-02 12:39:34 +0100

    add failing test

--- tests/testthat/test-target_sda.R
+++ tests/testthat/test-target_sda.R
@@ -487,3 +487,54 @@ test_that("with multiple values of `country_of_domicile` outputs the expected
 
   expect_equal(out$projected$emission_factor_value, 0.5)
 })
+
+test_that("outputs same target regardless of years present in ald", {
+
+  matched <- fake_matched(
+    name_ald = "company a",
+    sector_ald = "steel"
+  )
+
+
+  ald_ten_year <- fake_ald(
+    sector = "steel",
+    technology = "steel",
+    name_company = c(rep("company a", 3), rep("company b", 3)),
+    emission_factor = c(rep(1.5, 3), rep(2.5, 3)),
+    year = rep(c(2020, 2025, 2030),2),
+    plant_location = "DE"
+  )
+
+  ald_thirty_year <- fake_ald(
+    sector = "steel",
+    technology = "steel",
+    name_company = c(rep("company a", 4), rep("company b", 4)),
+    emission_factor = c(rep(1.5, 4), rep(2.5, 4)),
+    year = rep(c(2020, 2025, 2030, 2050),2),
+    plant_location = "DE"
+  )
+
+  co2_scenario <- fake_co2_scenario(
+    sector = "steel",
+    year = c(2020, 2025, 2030, 2050),
+    emission_factor = c(2, 1.9, 1.8, 0.25)
+  )
+
+  out_ten_year <- target_sda(matched, ald_ten_year, co2_scenario) %>%
+    filter(
+      year == 2030,
+      emission_factor_metric == "target_b2ds"
+    )
+
+  out_thirty_year <- target_sda(matched, ald_thirty_year, co2_scenario) %>%
+    filter(
+      year == 2030,
+      emission_factor_metric == "target_b2ds"
+    )
+
+  expect_equal(
+    out_ten_year$emission_factor_value,
+    out_thirty_year$emission_factor_value
+    )
+})
+

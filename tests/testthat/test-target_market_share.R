499a0407d24212df99671aa8e7f23d9a8a8b4a74
Jackson Hoffart <jackson.hoffart@gmail.com>
2021-07-08 12:42:43 +0200

    Unexpected input sectors warns gracefully (#333)

--- tests/testthat/test-target_market_share.R
+++ tests/testthat/test-target_market_share.R
@@ -1010,3 +1010,28 @@ test_that("technology_share is calculated per region (#315)", {
     c(0.5, 0.5)
   )
 })
+
+test_that("Input with unexpected sectors errors gracefully (#329)", {
+
+  matched <- fake_matched(
+    sector_ald = "a"
+  )
+
+  ald <- fake_ald(
+    sector = "a"
+  )
+
+  scenario <- fake_scenario(
+    sector = "a"
+  )
+
+  expect_warning(
+    target_market_share(
+      matched,
+      ald,
+      scenario,
+      region_isos_stable
+    ),
+    class = "has_zero_rows"
+  )
+})

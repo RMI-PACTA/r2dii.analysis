1037ef9aa97be81c78c2abe49a245b7148b915de
Mauro Lepore <maurolepore@gmail.com>
2021-07-31 08:30:30 -0600

    Move and prune r2dii.data

--- data-raw/sysdata.R
+++ data-raw/sysdata.R
@@ -5,52 +5,38 @@
 # system is updated (see #227).
 
 library(dplyr, warn.conflicts = FALSE)
-library(glue, warn.conflicts = FALSE)
-library(fs)
 library(withr)
 library(r2dii.match)
 
-use_r2dii_data <- function(version) {
-  library("r2dii.data", lib.loc = test_path("test_lib"))
-  on.exit(unloadNamespace("r2dii.data"), add = TRUE)
-  stopifnot(packageVersion("r2dii.data") == version)
+local({
+  devtools::load_all(here::here("data-raw/r2dii.data-0.1.4-pruned/"))
+  defer(unloadNamespace("r2dii.data"))
+  stopifnot(packageVersion("r2dii.data") == "0.1.4")
 
-  region <- r2dii.data::region_isos_demo
+  region_isos_stable <- region_isos_demo
 
-  loanbook_demo <- r2dii.data::loanbook_demo %>%
+  loanbook_demo <- loanbook_demo %>%
     mutate(across(.data$sector_classification_direct_loantaker, as.character))
 
-  nace_classification <- r2dii.data::nace_classification %>%
+  nace_classification <- nace_classification %>%
     select(.data$code, .data$sector, .data$borderline)
 
-  loanbook <- nace_classification %>%
+  loanbook_stable <- nace_classification %>%
     rename(sector_classification_direct_loantaker = .data$code) %>%
     mutate(across(sector_classification_direct_loantaker, as.character)) %>%
     right_join(loanbook_demo)
 
   local_options(r2dii.match.allow_reserved_columns = TRUE)
-  matched <- match_name(loanbook, r2dii.data::ald_demo) %>%
+  matched_stable <- match_name(loanbook_stable, ald_demo) %>%
     prioritize()
 
-  list(
-    region = region,
-    loanbook = loanbook,
-    matched = matched
+  usethis::use_data(
+    region_isos_stable,
+    loanbook_stable,
+    matched_stable,
+    internal = TRUE,
+    overwrite = TRUE
   )
-}
+})
 
-stable <- use_r2dii_data(version = "0.1.4")
-region_isos_stable <- stable$region
-loanbook_stable <- stable$loanbook
-matched_stable <- stable$matched
-
-usethis::use_data(
-  region_isos_stable,
-  loanbook_stable,
-  matched_stable,
-  internal = TRUE,
-  overwrite = TRUE
-)
-
-installed_version_remains_the_same <- packageVersion("r2dii.data") > "0.1.4"
-stopifnot(installed_version_remains_the_same)
+stopifnot(packageVersion("r2dii.data") > "0.1.4")

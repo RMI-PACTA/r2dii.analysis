7f3082e8cd44853d59767e0f5abc83fd99eb102a
Jackson Hoffart <jackson.hoffart@gmail.com>
2022-04-19 15:44:33 +0200

    `target_sda` gains `region_isos` argument (#397)

--- tests/testthat/test-target_market_share.R
+++ tests/testthat/test-target_market_share.R
@@ -488,7 +488,7 @@ test_that("w/ known input, outputs `technology_share` as expected (#184, #262)",
     c(0.6875, 0.3125)
   )
 
-  FIXME <- 1e-2  # Do we need 1e-3?
+  FIXME <- 1e-2 # Do we need 1e-3?
   expect_equal(
     out$corporate_economy$technology_share,
     c(0.666, 0.333),
@@ -1356,7 +1356,7 @@ test_that("w/ ald with older years than scenarios, outputs 0 percent change in
   expect_equal(
     initial_out$corporate_economy$percentage_of_initial_production_by_scope,
     0L
-    )
+  )
 
   expect_equal(
     initial_out$projected$percentage_of_initial_production_by_scope,
@@ -1367,5 +1367,4 @@ test_that("w/ ald with older years than scenarios, outputs 0 percent change in
     initial_out$target_sds$percentage_of_initial_production_by_scope,
     0L
   )
-
 })

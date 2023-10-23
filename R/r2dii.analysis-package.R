a68a2b65f9c4c24fa3d341bde9405b14ca89c077
Alex Axthelm <aaxthelm@RMI.org>
2023-10-23 16:50:01 +0200

    Add .onLoad to ensure that CRAN checks don't yuse more than 2 CPUs

--- R/r2dii.analysis-package.R
+++ R/r2dii.analysis-package.R
@@ -1,6 +1,15 @@
 #' @keywords internal
 "_PACKAGE"
 
+.onLoad <- function(libname, pkgname) {
+  # CRAN OMP THREAD LIMIT
+  # logic for setting OMP_THREAD_LIMIT form https://stackoverflow.com/a/77323812
+  # check modified from testthat's on_cran function
+  if (!interactive() && isFALSE(as.logical(Sys.getenv("NOT_CRAN", "false")))) {
+    Sys.setenv("OMP_THREAD_LIMIT" = 2)
+  }
+}
+
 # The following block is used by usethis to automatically manage
 # roxygen namespace tags. Modify with care!
 ## usethis namespace: start

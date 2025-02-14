17517e46b71cd06b253b3e2c15fc431b0ec53bf2
CJ Yetman <cj@cjyetman.com>
2025-02-14 11:33:26 +0100

    add tests to ensure that the data dictionary is up to date and complete

--- data-raw/build_data_dictionary.R
+++ data-raw/build_data_dictionary.R
@@ -4,6 +4,6 @@ library(usethis)
 paths <- list.files("data-raw/data_dictionary", full.names = TRUE)
 
 out <- readr::read_csv(file = paths, show_col_types = FALSE)
-data_dictionary <- out[order(out$dataset, out$column), , drop = FALSE]
+data_dictionary <- out[order(out[["dataset"]], out[["column"]]), , drop = FALSE]
 
 usethis::use_data(data_dictionary, overwrite = TRUE)
